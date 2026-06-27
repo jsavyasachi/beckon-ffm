aaffe09235d6f3cd92a720dcdf409b3e4779f864
Savyasachi <jsavyasachi@gmail.com>
2026-06-26 21:06:33 -0700

    chore: specify EPL version 1.0 and https in license metadata

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject net.clojars.savya/beckon-ffm "0.1.1"
   :description "Experimental Foreign Function & Memory signal backends for beckon: Linux signalfd and macOS/BSD kqueue."
   :url "https://github.com/jsavyasachi/beckon-ffm"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "Eclipse Public License 1.0"
+            :url "https://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.12.5"]
                  [net.clojars.savya/beckon "0.4.0"]]
   :java-source-paths ["src/java"]
